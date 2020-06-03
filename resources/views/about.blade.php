1306ed93f295df9419fecf16e622feca403ef1f2
AIRZONESL\crivera <crivera@altracorporacion.es>
2020-06-03 12:19:13 +0200

    about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+@section('content')
+
+<div class="alert alert-primary" role="alert">
+  About - sobre nosotros
+</div>
+
+<div >
+Boqueron WORLD nace en Málaga, el año 220 como practica del curso SCRUM, con el objeto de ofrecer un servicio del boquerón.
+</div>
+
+@stop
